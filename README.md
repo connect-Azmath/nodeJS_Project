1b7cda9487b6e92aa0d316e1eead2cf7cad40fec
connect-azmath <azmath900@gmail.com>
2023-08-02 20:55:39 +0530

    Updated files

--- README.md
+++ README.md
@@ -2,10 +2,29 @@
 nodeJS Project
 
 #Running server options
-npm run custom script name
+npm run custom-script-name
 npm start
 node server.js
+npm run dev (after nodemon is installed)
  
-# https://www.npmjs.com/
+## https://www.npmjs.com/
 nodemon - Auto restarts the node app whenever code is updated
 npm i nodemon
+
+### HTTP Methods
+GET     || Get certian amount of data from Server
+POST    || Sending data to server
+PUT     || Ovrwriting / full update
+DELETE  || Deleting data from Server
+PATCH   || Partial update
+
+### Server CODES
+200     || OK
+201     || Insertion / creation
+400     || Server Error
+401     || Bad Request
+403     || Forbidden ???
+404     || Page not found
+501     || Not Implemented
+502     || 
+503     || Service not available
--- server.js
+++ server.js
@@ -1,13 +1,44 @@
-const http = require("http");
+const http = require("http");   // package import
 
 const port = 8081; 
 
-http.createServer((require, response) => {
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("<h2>Hi World, Server Started </h2>");
-    response.end();
+const toDoList = ["java", "javascript", "python"];
+
+http.createServer((req, res) => {
+    const{method, url} = req;
+    console.log(method , url);
+
+    if(url === '/todos'){
+        if(method === 'GET'){
+            res.writeHead(200);
+            res.write(toDoList.toString());
+        }
+    } else if (url === '/azmath'){
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.write("<h2> Default page </h2>");
+
+    } else if (url === '/any'){
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.write("<h2> Default page </h2>");
+
+    }else if (url === '/'){
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.write("<h2> Default page </h2>");
+
+    }  else {
+        res.writeHead(501); // error code
+    }
+    res.end();
 })
+
+// http.createServer((require, response) => {
+//    // callback function
+//     response.writeHead(200, {"Content-Type": "text/html"});
+//     response.write("<h2>Hi World, Server Started </h2>");
+//     response.end();
+// })
 .listen(port, () => {
+     // callback function
     console.log('NodeJS server running on port number ${port}', port);
 } );
  
